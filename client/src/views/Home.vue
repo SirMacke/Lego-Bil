ab0d3a010d7f6fcf53da7b67b97ede951a11b997
Maximilian Helmersson <max.helmersson@gmail.com>
2021-06-03 13:05:35 +0200

    Added buttons

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,10 +1,16 @@
 <template>
   <main>
-    <div>
-      <input v-model="state.direction" type="range" id="direction" name="direction" min="-100" max="100">
-    </div>
-    <div>
-      <input v-model="state.speed" type="range" id="speed" name="speed" min="-100" max="100">
+    <h1>DriverBot</h1>
+    <div id="container">
+      <div>
+        <div id="direction">
+          <button @click="changeDirection(20)">Left</button>
+          <button @click="changeDirection(-20)">Right</button>
+        </div>
+      </div>
+      <div class="slidecontainer">
+        <input v-model="state.speed" type="range" class="slider" id="speed" name="speed" min="-100" max="100">
+      </div>
     </div>
   </main>
   <div id="login-signup">
@@ -25,9 +31,39 @@ export default {
         id: '123',
         adress: "maximilian.helmersson@abbindustrigymnasium.se"
       },
-      direction: 0,
-      speed: 0
+      speed: 0,
+      arrowUp: false,
+      arrowDown: false
     });
+
+    document.addEventListener("keydown", event => {
+      keyUpdate(event.key, true)
+    });
+    document.addEventListener("keyup", event => {
+      keyUpdate(event.key, false)
+    });
+
+    function keyUpdate (key, keyDown) {
+      switch (key) {
+        case "d":
+          if (!keyDown) changeDirection(-20);
+          break;
+        case "a":
+          if (!keyDown) changeDirection(20);
+          break;
+        case "w":
+          state.arrowUp = keyDown;
+          break;
+        case "s":
+          state.arrowDown = keyDown;
+          break;
+      }
+    }
+
+    setInterval(() => {
+      if (state.arrowUp) state.speed++;
+      if (state.arrowDown) state.speed--;
+    }, 10);
 
     const mqttConnect = () => {
       mqtt.launch(state.user.id, (topic, source) => {
@@ -36,10 +72,14 @@ export default {
       //mqtt.subscribe({ 'ping': 1 });
       setInterval(() => {
         mqtt.publish(state.user.adress + "/speed", parseInt(state.speed));
-        mqtt.publish(state.user.adress + "/direction", parseInt(state.direction));
       }, 500);     
     }
     mqttConnect();
+
+    function changeDirection(value) {
+      console.log(value)
+      mqtt.publish(state.user.adress + "/direction", parseInt(value));
+    }
 
     const mqttDisconnect = () => {
       mqtt.end()
@@ -48,7 +88,8 @@ export default {
     return {
       state,
       mqttConnect,
-      mqttDisconnect
+      mqttDisconnect,
+      changeDirection
     }
   }
 }
@@ -56,44 +97,95 @@ export default {
 
 <style lang="sass" scoped>
 main
-  display: flex
   overflow: hidden
 
   @media screen and (max-width: 800px)
     flex-direction: column
 
-  div
-    width: 50vw
-    height: 100vh
-    position: relative
+  h1
+    position: absolute
+    left: 50%
+    transform: translateX(-50%)
+    top: 25px
 
-    @media screen and (max-width: 800px)
-      width: 100vw
-      height: 50vh
+  #container
+    display: flex
 
-    #direction
-      position: absolute
-      width: 70%
-      left: 50%
-      top: 50%
-      transform: translate(-50%, -50%)
+    div
+      width: 50vw
+      height: 100vh
+      position: relative
 
       @media screen and (max-width: 800px)
-        transform: rotateZ(-90deg)
-        left: 15%
+        width: 100vw
+        height: 50vh
 
-    #speed
-      position: absolute
-      width: 70%
-      left: 25%
-      top: 50%
-      transform: rotateZ(-90deg)
-
-      @media screen and (max-width: 800px)
+      #direction
+        display: flex
+        position: absolute
         left: 50%
-        transform: rotateZ(0deg)
+        top: 50%
         transform: translate(-50%, -50%)
+        width: auto
+        height: auto
 
+      button
+        height: 60px
+        width: 100px
+        margin: 20px
+        border: none
+        border-radius: 5px
+        background: #d3d3d3
+        transition: opacity .2s
+        -webkit-transition: .2s 
+        font-size: 0.9em
+
+      button:hover
+        background: #04AA6D
+        cursor: pointer
+
+    .slidecontainer
+
+      /* The slider itself */
+      #speed
+        -webkit-appearance: none  /* Override default CSS styles */
+        appearance: none
+        width: 100% /* Full-width */
+        height: 25px /* Specified height */
+        background: #d3d3d3 /* Grey background */
+        outline: none /* Remove outline */
+        opacity: 0.7 /* Set transparency (for mouse-over effects on hover) */
+        -webkit-transition: .2s /* 0.2 seconds transition on hover */
+        transition: opacity .2s
+        position: absolute
+        width: 60%
+        left: 25%
+        top: 50%
+        transform: rotateZ(-90deg)
+
+        @media screen and (max-width: 800px)
+          left: 50%
+          transform: rotateZ(0deg)
+          transform: translate(-50%, -50%)
+
+      /* Mouse-over effects */
+      #speed:hover
+        opacity: 1 /* Fully shown on mouse-over */
+
+      /* The slider handle (use -webkit- (Chrome, Opera, Safari, Edge) and -moz- (Firefox) to override default look) */
+      #speed::-webkit-slider-thumb
+        -webkit-appearance: none /* Override default look */
+        appearance: none
+        width: 25px /* Set a specific slider handle width */
+        height: 25px /* Slider handle height */
+        background: #04AA6D /* Green background */
+        cursor: pointer /* Cursor on hover */
+
+      #speed::-moz-range-thumb
+        width: 25px /* Set a specific slider handle width */
+        height: 25px /* Slider handle height */
+        background: #04AA6D /* Green background */
+        cursor: pointer /* Cursor on hover */
 
 
 #login-signup
